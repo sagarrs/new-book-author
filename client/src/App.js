22da58c899bb543bcbecda722d51e78d508a5960
sagar <sagarrs293@gmail.com>
2019-05-21 03:10:51 +0530

    login page update

--- client/src/App.js
+++ client/src/App.js
@@ -13,47 +13,60 @@ function App(props) {
   console.log(props.users)
   return (
     <BrowserRouter>
-        <div>
+        <div className="container">
+          {/* <nav>
+              <ul>
+                <li className="logo">Author</li>
+              </ul>
+            
+              <ul>
+                <li><Link to="/">  Home </Link></li>
+                <li><Link to="/about">  About </Link></li>
+                <li><Link to="/mission">  Mission </Link></li>
+                <li><Link to="/contact">  Contact </Link></li>
+                <li><Link to="/login">  Login </Link></li>
+              </ul>  
+          </nav> */}
 
-        <Link to="/">Home</Link>
-        {
-          isAuthenticated(props.users) &&(
-            <div>
-              logged in
-              <Link to="/logout">Logout</Link>
-            </div>
-          )
-        }
-
-        {
-          !isAuthenticated(props.users) &&(
-            <div>
-              logged in
-              <Link to="/login">Login</Link><br/>
-              <Link to="/register">Register</Link><br/>
-            </div>
-          )
-        }
-
-            {/* <Link to="/logout">Logout</Link><br/>
-            <Link to="/login">Login</Link><br/>
-            <Link to="/register">Register</Link><br/> */}
-
-          <Switch>
-            <Route path="/" component={Home} exact={true}/>
-            <Route path="/login" component={LoginForm} exact={true}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/story" component={Story}/>
-
-            <Route path="/logout" component={() => {
-              return (
+          <Link to="/">Home</Link>
+            {
+              isAuthenticated(props.users) &&(
                 <div>
-                  {props.dispatch(startLogout())}
-                  <Link to="/" className="btn btn-outline-success">Redirect To Home</Link>
+                  logged in
+                  <Link to="/logout">Logout</Link>
                 </div>
               )
-            }}/>
-          </Switch>
+            }
+
+            {
+              !isAuthenticated(props.users) &&(
+                <div>
+                  logged in
+                  <Link to="/login">Login</Link><br/>
+                  <Link to="/register">Register</Link><br/>
+                </div>
+              )
+            }
+
+              {/* <Link to="/logout">Logout</Link><br/>
+              <Link to="/login">Login</Link><br/>
+              <Link to="/register">Register</Link><br/> */}
+
+            <Switch>
+              <Route path="/" component={Home} exact={true}/>
+              <Route path="/login" component={LoginForm} exact={true}/>
+              <Route path="/register" component={Register}/>
+              <Route path="/story" component={Story}/>
+
+              <Route path="/logout" component={() => {
+                return (
+                  <div>
+                    {props.dispatch(startLogout())}
+                    <Link to="/" className="btn btn-outline-success">Redirect To Home</Link>
+                  </div>
+                )
+              }}/>
+            </Switch>
         </div>
     </BrowserRouter>
   );
